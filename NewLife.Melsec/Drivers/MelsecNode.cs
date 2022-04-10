4377ebde2373771477e2c72c7e746642e60f655a
笑笑 <xxred0324@qq.com>
2022-04-10 18:50:38 +0800

    初步完成三菱PLC驱动

---
+++ NewLife.Melsec/Drivers/MelsecNode.cs
@@ -0,0 +1,15 @@
+﻿using NewLife.IoT.Protocols;
+
+namespace NewLife.IoT.Drivers;
+
+/// <summary>
+/// 节点
+/// </summary>
+public class MelsecNode : INode
+{
+    /// <summary>主机地址</summary>
+    public String Address { get; set; }
+
+    /// <summary>通道</summary>
+    public IChannel Channel { get; set; }
+}
