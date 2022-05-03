d3804c366b70655bd944693fcd68fed56fbf6cb3
大石头 <nnhy@vip.qq.com>
2022-05-03 18:15:20 +0800

    驱动参数增加备注，生成参数模版Xml时将会带上注释

--- NewLife.Melsec/Drivers/MelsecParameter.cs
+++ NewLife.Melsec/Drivers/MelsecParameter.cs
@@ -1,13 +1,16 @@
-﻿using NewLife.IoT.Drivers;
+﻿using System.ComponentModel;
+using NewLife.IoT.Drivers;
 
 namespace NewLife.Melsec.Drivers;
 
 /// <summary>Melsec参数</summary>
 public class MelsecParameter : IDriverParameter
 {
-    /// <summary>地址</summary>
+    /// <summary>地址。例如 127.0.0.1:6000</summary>
+    [Description("地址。例如 127.0.0.1:6000")]
     public String Address { get; set; }
 
     /// <summary>数据格式。ABCD/BADC/CDAB/DCBA</summary>
+    [Description("数据格式。ABCD/BADC/CDAB/DCBA")]
     public String DataFormat { get; set; }
 }
