53db27e28b126c39eca5b8abfa70d79a1129b4ef
大石头 <nnhy@vip.qq.com>
2022-11-03 17:57:42 +0800

    测试三菱PLC串口通信

---
+++ Test2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Ports;
+using System.Xml;
+using NewLife;
+using NewLife.Data;
+using NewLife.Log;
+
+XTrace.UseConsole();
+
+try
+{
+    var sp = new SerialPort("COM7", 9600)
+    {
+        DataBits = 7,
+        StopBits = StopBits.One,
+        Parity = Parity.Even,
+
+        ReadTimeout = 3000,
+        WriteTimeout = 3000
+    };
+    sp.Open();
+
+    var str = "02 30 31 31 39 34 30 32 03 36 34";
+    var buf = str.ToHex();
+    //var buf = str.GetBytes();
+
+    XTrace.WriteLine("send {0}", buf.ToHex("-"));
+
+    sp.Write(buf, 0, buf.Length);
+
+    Thread.Sleep(50);
+
+    var rs = new Byte[256];
+    var count = sp.Read(buf, 0, buf.Length);
+
+    var pk = new Packet(rs, 0, count);
+
+    XTrace.WriteLine("recv {0}", pk.ToHex(256, "-"));
+}
+catch (Exception ex)
+{
+    XTrace.WriteException(ex);
+}
